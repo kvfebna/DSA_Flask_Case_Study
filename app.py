252331b45dc44533a95eb9c5985b2aae554ce130
FEBNA KV <156453863+kvfebna@users.noreply.github.com>
2024-04-23 12:14:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask,request,render_template
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+with open('model.pkl','rb') as model_file:
+    model=pickle.load(model_file)
+
+with open('x.pkl','rb') as x:
+    x=pickle.load(x)   
+
+sc=StandardScaler()
+scc=sc.fit(x) 
+
+app=Flask(__name__)
+
+@app.after_request
+def add_security_headers(resp):
+    resp.headers['Content-Security-Policy'] = "default-src 'self';"
+    return resp
+                      
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+   Gend=request.form['Gender']
+   if Gend=="Male":
+     Gender=1
+   elif Gend=='Female':
+     Gender=0
+   EstimatedSalary=int(request.form['EstimatedSalary'])
+   Age=int(request.form['Age'])
+   feature=np.array([[Gender,Age,EstimatedSalary]])
+   feature_sc=scc.transform(feature)
+   prediction=model.predict(feature_sc)
+   return render_template('index.html',pred_res=prediction[0])
+
+   
+if __name__=='__main__':
+  app.run(debug=True)
